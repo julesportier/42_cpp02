471cf03331c900e77e9ab159fc176edfe4e65533
jules <142106821+julesportier@users.noreply.github.com>
2025-08-26 17:37:06 +0200

    feat(ex02): comparison operators

---
+++ ex02/src/Fixed.cpp
@@ -0,0 +1,99 @@
+#include "Fixed.h"
+#include <iostream>
+#include <cmath>
+
+const int Fixed::m_fract_bits = 8;
+
+/******************
+ * PUBLIC METHODS *
+ * ***************/
+Fixed::Fixed() : m_fpi(0)
+{
+	std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(const int i) : m_fpi(i << m_fract_bits)
+{
+	std::cout << "Int constructor called\n";
+}
+
+Fixed::Fixed(const float f) : m_fpi(roundf(f * (1 << (m_fract_bits))))
+{
+	std::cout << "Float constructor called\n";
+}
+
+Fixed::Fixed(const Fixed& src) : m_fpi(src.getRawBits())
+{
+	std::cout << "Copy constructor called\n";
+}
+
+Fixed& Fixed::operator=(const Fixed& src)
+{
+	std::cout << "Copy assignement operator called\n";
+	if (this != &src)
+		m_fpi = src.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called\n";
+}
+
+int Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called\n";
+	return (m_fpi);
+}
+
+void Fixed::setRawBits(const int raw)
+{
+	std::cout << "setRawBits member function called\n";
+	m_fpi = raw;
+}
+
+float Fixed::toFloat() const
+{
+	return ((float)m_fpi / (float)(1 << m_fract_bits));
+}
+
+float Fixed::toInt() const
+{
+	return (m_fpi >> m_fract_bits);
+}
+
+bool Fixed::operator<(const Fixed& rhs) const
+{
+	return (m_fpi < rhs.getRawBits());
+}
+
+bool Fixed::operator>(const Fixed& rhs) const
+{
+	return (rhs < *this);
+}
+
+bool Fixed::operator<=(const Fixed& rhs) const
+{
+	return (!(*this > rhs));
+}
+
+bool Fixed::operator>=(const Fixed& rhs) const
+{
+	return (!(*this < rhs));
+}
+
+bool Fixed::operator==(const Fixed& rhs) const
+{
+	return (m_fpi ==  rhs.getRawBits());
+}
+
+bool Fixed::operator!=(const Fixed& rhs) const
+{
+	return (!(*this == rhs));
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& i)
+{
+	os << i.toFloat();
+	return (os);
+}
---
+++ ex02/src/Fixed.h
@@ -0,0 +1,31 @@
+#ifndef FIXED_H
+# define FIXED_H
+
+# include <iostream>
+
+class Fixed {
+public:
+	Fixed();
+	Fixed(const int i);
+	Fixed(const float f);
+	Fixed(const Fixed& src);
+	~Fixed();
+	Fixed& operator=(const Fixed& src);
+	int getRawBits() const;
+	void setRawBits(const int raw);
+	float toFloat() const;
+	float toInt() const;
+	bool operator<(const Fixed& rhs) const;
+	bool operator>(const Fixed& rhs) const;
+	bool operator<=(const Fixed& rhs) const;
+	bool operator>=(const Fixed& rhs) const;
+	bool operator==(const Fixed& rhs) const;
+	bool operator!=(const Fixed& rhs) const;
+private:
+	int m_fpi;
+	static const int m_fract_bits;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& i);
+
+#endif
