c503e6e4e31f90e6dcd0167b034f79a72daed984
jules <142106821+julesportier@users.noreply.github.com>
2025-08-27 10:27:36 +0200

    ref(ex02): disable constructors etc prints

--- ex02/src/Fixed.cpp
+++ ex02/src/Fixed.cpp
@@ -9,27 +9,27 @@ const int Fixed::m_fract_bits = 8;
  * ***************/
 Fixed::Fixed() : m_fpi(0)
 {
-	std::cout << "Default constructor called\n";
+	// std::cout << "Default constructor called\n";
 }
 
 Fixed::Fixed(const int i) : m_fpi(i << m_fract_bits)
 {
-	std::cout << "Int constructor called\n";
+	// std::cout << "Int constructor called\n";
 }
 
 Fixed::Fixed(const float f) : m_fpi(roundf(f * (1 << (m_fract_bits))))
 {
-	std::cout << "Float constructor called\n";
+	// std::cout << "Float constructor called\n";
 }
 
 Fixed::Fixed(const Fixed& src) : m_fpi(src.getRawBits())
 {
-	std::cout << "Copy constructor called\n";
+	// std::cout << "Copy constructor called\n";
 }
 
 Fixed& Fixed::operator=(const Fixed& src)
 {
-	std::cout << "Copy assignement operator called\n";
+	// std::cout << "Copy assignement operator called\n";
 	if (this != &src)
 		m_fpi = src.getRawBits();
 	return (*this);
@@ -37,18 +37,18 @@ Fixed& Fixed::operator=(const Fixed& src)
 
 Fixed::~Fixed()
 {
-	std::cout << "Destructor called\n";
+	// std::cout << "Destructor called\n";
 }
 
 int Fixed::getRawBits() const
 {
-	std::cout << "getRawBits member function called\n";
+	// std::cout << "getRawBits member function called\n";
 	return (m_fpi);
 }
 
 void Fixed::setRawBits(const int raw)
 {
-	std::cout << "setRawBits member function called\n";
+	// std::cout << "setRawBits member function called\n";
 	m_fpi = raw;
 }
 
